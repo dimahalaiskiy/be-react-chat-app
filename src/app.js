a0d590fa8c4007d0e84c245565d68e2f178bb05f
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-11 16:01:25 +0300

    ADDED: env port

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const authRouter = require('./routes/auth');
 require('./database/index.js');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
