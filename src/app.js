e2ec684cc70bb2a70c558c6602af629ebb01781a
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-04-16 03:29:17 +0300

    MERGED: Dev into main

--- src/app.js
+++ src/app.js
@@ -25,15 +25,11 @@ app.use(
 );
 
 app.use((req, res, next) => {
-<<<<<<< HEAD
   res.setHeader(
     'Access-Control-Allow-Origin',
     'https://react-chap-app.vercel.app'
   );
-=======
->>>>>>> dev
 res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
   next();
 });
 
@@ -43,6 +39,7 @@ app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
+    name: 'session',
     secret: 'SECRETKEYasd',
     resave: false,
     saveUninitialized: false,
@@ -51,15 +48,10 @@ app.use(
         'mongodb+srv://dimagalaiskiy:qwerty123@learnmongo.pxcxty7.mongodb.net/test',
     }),
     cookie: {
-<<<<<<< HEAD
-      expires: new Date('2023-04-15T00:00:00.000Z'),
-      ameSite: 'strict',
-=======
       maxAge: 3600000,
       httpOnly: false,
       secure: true,
       sameSite: 'none',
->>>>>>> dev
     },
   })
 );
