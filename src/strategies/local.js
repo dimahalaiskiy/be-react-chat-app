5061fe745fe8f86874f16c5b90888eeafcad4c82
Dmytro Halaiskiy <dmytrohalaiskiy@dao-group.org>
2023-07-25 17:22:21 +0300

    UPDATE: Photo upload + login

--- src/strategies/local.js
+++ src/strategies/local.js
@@ -1,21 +1,21 @@
-const passport = require('passport');
-const { Strategy } = require('passport-local');
-const User = require('../database/schemas/User');
-const { comparePassword } = require('../utils/helpers');
+const passport = require("passport");
+const { Strategy } = require("passport-local");
+const User = require("../database/schemas/User");
+const { comparePassword } = require("../utils/helpers");
 
 passport.serializeUser((user, done) => {
-  console.log('serialize user');
+  console.log("serialize user");
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-  console.log('deserialize user');
+  console.log("deserialize user");
   try {
     const user = await User.findById(id);
-    if (!user) throw new Error('User not found!');
+    if (!user) throw new Error("User not found!");
     done(null, user);
   } catch (error) {
-    console.log('error', error);
+    console.log("error", error);
     done(error, null);
   }
 });
@@ -23,24 +23,24 @@ passport.deserializeUser(async (id, done) => {
 passport.use(
   new Strategy(
     {
-      usernameField: 'email',
+      usernameField: "email",
     },
     async (email, password, done) => {
       try {
         if (!email || !password)
-          throw new Error('Bad request. Missing credentials');
+          throw new Error("Bad request. Missing credentials");
         const userDB = await User.findOne({ email });
-        if (!userDB) throw new Error('User not found');
+        if (!userDB) throw new Error("User not found");
         const isValid = comparePassword(password, userDB.password);
         if (isValid) {
-          console.log('Authenticated Successfully!');
+          console.log("Authenticated Successfully!");
           done(null, userDB);
         } else {
-          console.log('Invalid Authentication');
+          console.log("Invalid Authentication");
           done(null, null);
         }
       } catch (error) {
-        console.log('error', error);
+        console.log("error", error);
         done(error, null);
       }
     }
